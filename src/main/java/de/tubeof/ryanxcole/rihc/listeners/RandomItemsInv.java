0aaef3454cd0147cb7b9a52b2ba355444b7b1449
ThoD4v <thod4v@yourgamespace.com>
2022-04-19 19:37:55 +0200

    1.1.1 Release
    - Fixed a bug

--- src/main/java/de/tubeof/ryanxcole/rihc/listeners/RandomItemsInv.java
+++ src/main/java/de/tubeof/ryanxcole/rihc/listeners/RandomItemsInv.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -44,5 +45,10 @@ public class RandomItemsInv implements Listener {
 
         player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 2, 1);
         selectedItems.put(player.getUniqueId(), selected);
+    }
+
+    @EventHandler
+    public void onClose(InventoryCloseEvent event) {
+        selectedItems.remove(event.getPlayer().getUniqueId());
     }
 }
